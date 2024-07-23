fee6e0c98bd2775f5f45d7c5f1fc81b97c4f33b2
Asaph Ho Canyang <asaph.ho-cy@venture.com.sg>
2024-07-23 16:45:05 +0800

    main function done

--- main.py
+++ main.py
@@ -1,3 +1,54 @@
+from classes.game import Game
+
+intro_text = ('Enter moves in standard algebraic notation. Always use uppercase for non-pawn pieces. '
+              'Give all files in lowercase. \n '
+              'Commands: /showmoves /showfen /help')
 
 
+def main(game: Game):
+    print(intro_text)
+    while True:
+        to_move = game.current_position.to_move()
+        move_number = game.current_position.get_move_number()
+        prompt = f'{move_number}'
+        if to_move == 'white':
+            prompt += '. '
+        else:
+            prompt += '... '
+        input_str = input(prompt).strip()
 
+        if input_str.startswith('/'):
+            handle_command(game, input_str)
+
+        else:
+            try:
+                res = game.process_input_notation(input_str)
+                print(res)
+            except Exception:
+                continue
+            end_game_check = game.check_game_end_conditions()
+            if end_game_check != 'None':
+                print(end_game_check)
+                while True:
+                    command = input().strip()
+                    handle_command(game, command)
+
+
+def handle_command(game, input_str):
+    if input_str.lower() == '/showmoves':
+        game.show_moves()
+
+    elif input_str.lower() == '/showfen':
+        fen = game.current_position.generate_fen()
+        print(fen)
+
+    elif input_str.lower() == '/help':
+        print(intro_text)
+
+    else:
+        print('Unrecognized command.')
+
+
+if __name__ == '__main__':
+    game = Game()
+    main(game)
