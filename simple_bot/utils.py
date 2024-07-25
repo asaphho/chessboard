f7840b74ce7f35facd18b92c4550cc04ef31453b
Asaph Ho Canyang <asaphho93@gmail.com>
2024-07-25 16:41:02 +0800

    added function look_for_mate_in_one that looks for mate in one

--- simple_bot/utils.py
+++ simple_bot/utils.py
@@ -1,5 +1,6 @@
 from classes.position import Position
 from classes.move import LegalMove
+from typing import Union
 
 
 def branch_from_position(position: Position, move: LegalMove) -> Position:
@@ -24,4 +25,18 @@ def check_if_move_ends_game(current_position: Position, move: LegalMove) -> str:
     if new_position.is_under_check(to_move):
         return 'checkmate'
     return 'stalemate'
+
+
+def look_for_mate_in_one(current_position: Position, to_move: str) -> Union[LegalMove, None]:
+    """
+    Looks for a mate in one in the current position for the side to move.
+    :param current_position:
+    :param to_move: 'white' or 'black'
+    :return: a LegalMove object that delivers checkmate when played in the current position, or None if no mate exists.
+    """
+    all_legal_moves = current_position.get_all_legal_moves_for_color(to_move)
+    for move in all_legal_moves:
+        if check_if_move_ends_game(current_position, move) == 'checkmate':
+            return move
+    return None
 
