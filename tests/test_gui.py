39d7d76c4e799d7de7e516f52bb15b3cc8c28abc
Asaph Ho <asaphho@yahoo.com.sg>
2024-07-27 17:15:36 +0800

    WIP: gui main loop. TODO: process inputs, make functions in parse_notation put error messages in raised exceptions instead of calls to print.

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,26 +1,32 @@
 import PySimpleGUI as sg
-from classes.position import generate_starting_position
-from classes.move import LegalMove
+from classes.game import Game
+from typing import List
 from gui.utils import generate_position_layout
 
-position = generate_starting_position()
-position.process_legal_move(LegalMove(color='white', piece_type='pawn', origin_square='d2', destination_square='d4'))
-position.process_legal_move(LegalMove(color='black', piece_type='pawn', origin_square='e7', destination_square='e5'))
-position.process_legal_move(LegalMove(color='white', piece_type='pawn', origin_square='d4', destination_square='e5', is_capture=True))
-position.process_legal_move(LegalMove(color='black', piece_type='queen', origin_square='d8', destination_square='e7'))
-position.process_legal_move(LegalMove(color='white', piece_type='bishop', origin_square='c1', destination_square='f4'))
-position.process_legal_move(LegalMove(color='black', piece_type='queen', origin_square='e7', destination_square='b4'))
-position.process_legal_move(LegalMove(color='white', piece_type='bishop', origin_square='f4', destination_square='d2'))
-position.process_legal_move(LegalMove(color='black', piece_type='queen', origin_square='b4', destination_square='b2', is_capture=True))
-position.process_legal_move(LegalMove(color='white', piece_type='bishop', origin_square='d2', destination_square='c3'))
-position.process_legal_move(LegalMove(color='black', piece_type='bishop', origin_square='f8', destination_square='b4'))
-position.process_legal_move(LegalMove(color='white', piece_type='queen', origin_square='d1', destination_square='d2'))
-position.process_legal_move(LegalMove(color='black', piece_type='bishop', origin_square='b4', destination_square='c3', is_capture=True))
-position.process_legal_move(LegalMove(color='white', piece_type='queen', origin_square='d2', destination_square='c3', is_capture=True))
-position.process_legal_move(LegalMove(color='black', piece_type='queen', origin_square='b2', destination_square='c1'))
-position_layout = generate_position_layout(position)
-layout = position_layout + [[sg.Text(position.generate_fen())]]
-layout += [[sg.Button('Flip board')]]
+intro_text = ('Enter moves in standard algebraic notation. Always use uppercase for non-pawn pieces.\n '
+              'Give all files in lowercase. Do not include any spaces.\n ')
+# buttons: 'Flip board' 'Show moves' 'Show FEN' 'Restart game' 'Take back last move'
+game = Game()
+
+
+def generate_layout(game, output_from_prev_input: str = '') -> List[List]:
+    position = game.current_position
+    layout = [[sg.Text(intro_text)]]
+    position_layout = generate_position_layout(position)
+    layout += position_layout + [[sg.Text(output_from_prev_input, key='-TEXT-')]]
+    to_move = game.current_position.to_move()
+    move_number = game.current_position.get_move_number()
+    prompt = f'{move_number}'
+    if to_move == 'white':
+        prompt += '. '
+    else:
+        prompt += '... '
+    layout += [[sg.Text(prompt), sg.InputText(key='-INPUT-'), sg.Button('Enter move')]]
+    layout += [[sg.Button('Flip board'), sg.Button('Show moves'), sg.Button('Show FEN'), sg.Button('Restart game'), sg.Button('Take back last move')]]
+    return layout
+
+
+layout = generate_layout(game)
 window = sg.Window('Title', layout, element_padding=(0, 0))
 
 while True:
@@ -28,12 +34,16 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     if event == 'Flip board':
-        position.flip_position()
-        print(position.is_flipped())
-        position_layout = generate_position_layout(position)
-        layout = position_layout + [[sg.Text(position.generate_fen())]]
-        layout += [[sg.Button('Flip board')]]
+        game.current_position.flip_position()
+        text_value = window['-TEXT-'].DisplayText
+        new_layout = generate_layout(game, text_value)
         window.close()
-        window = sg.Window('Title', layout, element_padding=(0, 0))
+        window = sg.Window('Title', new_layout, element_padding=(0, 0))
+    elif event == 'Show moves':
+        moves = game.show_moves(return_string_for_window=True)
+        new_window_layout = [[sg.Text(moves)]]
+        new_window = sg.Window('Moves', new_window_layout, finalize=True)
+    elif event == 'Show FEN':
+        window['-TEXT-'].update(game.current_position.generate_fen())
 
 window.close()
