b4e1aff974e1f6c3a1f961dbbff1b4bc9251d958
Asaph Ho <asaphho@yahoo.com.sg>
2024-07-28 15:24:24 +0800

    Moved generate_position_layout and FEN_SYMBOL_TO_PIECE to gui_main.py and deleted gui directory

--- gui_main.py
+++ gui_main.py
@@ -1,16 +1,42 @@
+from os import path
+
 import PySimpleGUI as sg
 import PySimpleGUI.PySimpleGUI
+
+from classes.position import Position
+from images import IMAGES_FOLDER_PATH
+from utils.board_functions import square_color_int
 from version import software_version
 from classes.game import Game
 from typing import List
-from gui.utils import generate_position_layout
 
 TITLE = f'chessboard v{software_version}'
+FEN_SYMBOL_TO_PIECE = {'P': 'wpawn', 'K': 'wking', 'Q': 'wqueen', 'B': 'wbishop', 'N': 'wknight', 'R': 'wrook',
+                       'p': 'bpawn', 'k': 'bking', 'q': 'bqueen', 'b': 'bbishop', 'n': 'bknight', 'r': 'brook',
+                       '1': 'empty'}
 
 intro_text = ('Enter moves in standard algebraic notation. Always use uppercase for non-pawn pieces.\n '
               'Give all files in lowercase. Do not include any spaces.\n ')
 # buttons: 'Flip board' 'Show moves' 'Show FEN' 'Restart game' 'Take back last move'
 game = Game()
+
+
+def generate_position_layout(position: Position) -> List[List]:
+    ranks = '87654321' if not position.is_flipped() else '12345678'
+    files = 'abcdefgh'if not position.is_flipped() else 'hgfedcba'
+    layout = []
+    for r in ranks:
+        rank_layout = []
+        for f in files:
+            curr_square = f'{f}{r}'
+            square_color_bit = square_color_int(curr_square)
+            square_color = 'light' if square_color_bit == 0 else 'dark'
+            piece_at_square = position.look_at_square(curr_square)
+            filename = f'{square_color}_{FEN_SYMBOL_TO_PIECE[piece_at_square]}.png'
+            filepath = path.join(IMAGES_FOLDER_PATH, filename)
+            rank_layout.append(sg.Image(filepath, key=curr_square))
+        layout.append(rank_layout)
+    return layout
 
 
 def generate_layout(game: Game, output_from_prev_input: str = '', game_end_text: str = None) -> List[List]:
@@ -128,3 +154,9 @@ while True:
 
 
 window.close()
+
+
+
+
+
+
