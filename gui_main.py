ab31417e6fbe6eb08fda9bc194309120d8b75d86
Asaph Ho <asaphho@yahoo.com.sg>
2024-07-29 21:01:45 +0800

    v0.1.1: Window no longer closes and reopens after every move. Return key is bound to Enter move button.

--- gui_main.py
+++ gui_main.py
@@ -1,5 +1,5 @@
 from os import path
-
+from classes.move import LegalMove
 import PySimpleGUI as sg
 import PySimpleGUI.PySimpleGUI
 import sys
@@ -37,17 +37,67 @@ def generate_position_layout(position: Position) -> List[List]:
         rank_layout = []
         for f in files:
             curr_square = f'{f}{r}'
-            square_color_bit = square_color_int(curr_square)
-            square_color = 'light' if square_color_bit == 0 else 'dark'
+            square_color = get_square_color(curr_square)
             piece_at_square = position.look_at_square(curr_square)
             filename = f'{square_color}_{FEN_SYMBOL_TO_PIECE[piece_at_square]}.png'
-            try:
-                filepath = path.join(sys._MEIPASS, 'images', filename)
-            except Exception:
-                filepath = path.join('.', 'images', filename)
+            filepath = get_path_to_image(filename)
             rank_layout.append(sg.Image(filepath, key=curr_square))
         layout.append(rank_layout)
     return layout
+
+
+def get_square_color(curr_square: str) -> str:
+    square_color_bit = square_color_int(curr_square)
+    square_color = 'light' if square_color_bit == 0 else 'dark'
+    return square_color
+
+
+def get_path_to_image(filename: str) -> str:
+    try:
+        filepath = path.join(sys._MEIPASS, 'images', filename)
+    except Exception:
+        filepath = path.join('.', 'images', filename)
+    return filepath
+
+
+def update_position_layout_in_window(window: PySimpleGUI.PySimpleGUI.Window, move: LegalMove) -> None:
+    origin_square = move.origin_square
+    piece_color = move.get_color().lower()[0]
+    piece_type = move.piece_moved
+    destination_square = move.destination_square
+    destination_square_color = get_square_color(destination_square)
+    origin_square_color = get_square_color(origin_square)
+    origin_square_filename = f'{origin_square_color}_empty.png'
+    origin_square_filepath = get_path_to_image(origin_square_filename)
+    window[origin_square].update(filename=origin_square_filepath)
+    if not move.pawn_promotion_required():
+        destination_square_filename = f'{destination_square_color}_{piece_color}{piece_type}.png'
+    else:
+        destination_square_filename = f'{destination_square_color}_{piece_color}{move.promotion_piece}.png'
+    destination_square_filepath = get_path_to_image(destination_square_filename)
+    window[destination_square].update(filename=destination_square_filepath)
+    if move.castling != 'None':
+        rook_home_file = 'h' if move.castling == 'kingside' else 'a'
+        back_rank = '1' if piece_color == 'w' else '8'
+        rook_destination_file = 'f' if move.castling == 'kingside' else 'd'
+        rook_home_square = rook_home_file + back_rank
+        rook_home_square_color = get_square_color(rook_home_square)
+        rook_home_square_filename = f'{rook_home_square_color}_empty.png'
+        rook_home_square_filepath = get_path_to_image(rook_home_square_filename)
+        window[rook_home_square].update(filename=rook_home_square_filepath)
+        rook_destination_square = rook_destination_file + back_rank
+        rook_destination_square_color = get_square_color(rook_destination_square)
+        rook_destination_square_filename = f'{rook_destination_square_color}_{piece_color}rook.png'
+        rook_destination_square_filepath = get_path_to_image(rook_destination_square_filename)
+        window[rook_destination_square].update(filename=rook_destination_square_filepath)
+    if move.is_en_passant_capture():
+        captured_pawn_rank = '5' if piece_color == 'w' else '4'
+        captured_pawn_square = destination_square[0] + captured_pawn_rank
+        captured_pawn_square_color = get_square_color(captured_pawn_square)
+        captured_pawn_square_filename = f'{captured_pawn_square_color}_empty.png'
+        captured_pawn_square_filepath = get_path_to_image(captured_pawn_square_filename)
+        window[captured_pawn_square].update(filename=captured_pawn_square_filepath)
+    window.refresh()
 
 
 def generate_layout(game: Game, output_from_prev_input: str = '', game_end_text: str = None) -> List[List]:
@@ -85,15 +135,11 @@ def generate_layout(game: Game, output_from_prev_input: str = '', game_end_text:
             prompt += '. '
         else:
             prompt += '... '
-        layout += [[sg.Text(prompt), sg.InputText(key='-INPUT-'), sg.Button('Enter move')]]
+        layout += [[sg.Text(prompt), sg.InputText(key='-INPUT-', focus=True), sg.Button('Enter move', bind_return_key=True)]]
     else:
         layout += [[sg.Text(game_end_text)]]
     layout += [[sg.Button('Flip board'), sg.Button('Show moves'), sg.Button('Show FEN'), sg.Button('Restart game'), sg.Button('Take back last move')]]
     return layout
-
-
-layout = generate_layout(game)
-window = sg.Window(TITLE, layout, element_padding=(0, 0), return_keyboard_events=True, use_default_focus=False)
 
 
 def flip_board(game: Game, window: PySimpleGUI.PySimpleGUI.Window, game_end_text: str = None) -> PySimpleGUI.PySimpleGUI.Window:
@@ -101,8 +147,7 @@ def flip_board(game: Game, window: PySimpleGUI.PySimpleGUI.Window, game_end_text
     text_value = window['-TEXT-'].DisplayText
     new_layout = generate_layout(game, text_value, game_end_text)
     window.close()
-    new_window = sg.Window(TITLE, new_layout, element_padding=(0, 0), return_keyboard_events=(game_end_text is None),
-                           use_default_focus=False)
+    new_window = sg.Window(TITLE, new_layout, element_padding=(0, 0))
     return new_window
 
 
@@ -116,8 +161,7 @@ def perform_restart(game: Game, window: PySimpleGUI.PySimpleGUI.Window) -> PySim
     game.restart_game()
     new_layout = generate_layout(game, 'Game restarted.')
     window.close()
-    new_window = sg.Window(TITLE, new_layout, element_padding=(0, 0), return_keyboard_events=True,
-                           use_default_focus=False)
+    new_window = sg.Window(TITLE, new_layout, element_padding=(0, 0))
     return new_window
 
 
@@ -125,10 +169,12 @@ def perform_takeback(game: Game, window: PySimpleGUI.PySimpleGUI.Window) -> PySi
     takeback_result = game.take_back_last_move(silent=True)
     new_layout = generate_layout(game, takeback_result)
     window.close()
-    new_window = sg.Window(TITLE, new_layout, element_padding=(0, 0), return_keyboard_events=True,
-                           use_default_focus=False)
+    new_window = sg.Window(TITLE, new_layout, element_padding=(0, 0))
     return new_window
 
+
+layout = generate_layout(game)
+window = sg.Window(TITLE, layout, element_padding=(0, 0))
 
 while True:
     event, values = window.read()
@@ -145,21 +191,23 @@ while True:
             window = perform_restart(game, window)
     elif event == 'Take back last move':
         window = perform_takeback(game, window)
-    elif event == 'Enter move' or event.split(':')[0] == 'Return':
+    elif event == 'Enter move':
         input_notation = values['-INPUT-'].strip()
         if input_notation == '':
             continue
         try:
-            res = game.process_input_notation(input_notation)
+            res, move = game.process_input_notation(input_notation, return_move_for_gui=True)
         except Exception as e:
             window['-TEXT-'].update(str(e))
             continue
         game_end_check = game.check_game_end_conditions()
         if game_end_check == 'None':
-            new_layout = generate_layout(game, res)
-            window.close()
-            window = sg.Window(TITLE, new_layout, element_padding=(0, 0), return_keyboard_events=True,
-                               use_default_focus=False)
+            update_position_layout_in_window(window, move)
+            window['-TEXT-'].update(res)
+            window['-INPUT-'].update('')
+            # new_layout = generate_layout(game, res)
+            # window.close()
+            # window = sg.Window(TITLE, new_layout, element_padding=(0, 0))
         else:
             new_layout = generate_layout(game, res, game_end_check)
             window.close()
