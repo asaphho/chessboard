503f6e05082a8b764e9fdcceae6de841c1508979
Asaph Ho <asaphho@yahoo.com.sg>
2024-08-09 15:20:21 +0800

    functions for checking for hanging material to capture, and set evaluation to only be for the side that has just moved.

--- simple_bot/evaluation.py
+++ simple_bot/evaluation.py
@@ -3,7 +3,10 @@ from utils.board_functions import scan_qbr_scope, scan_kn_scope, get_intervening
 from classes.move import LegalMove
 from classes.position import Position, opposite_color
 from simple_bot.parameters import (MATERIAL_DICT, WHITE_PAWN_CONTROL_SCORES, BLACK_PAWN_CONTROL_SCORES,
-                                   LEGAL_PIECE_MOVE_MULTIPLIER, ACTIVITY_COUNT_MULTIPLIER)
+                                   ACTIVITY_COUNT_MULTIPLIER, CHECKMATE_SCORE)
+from simple_bot.utils import branch_from_position
+
+SYMBOL_TO_PIECE = {'P': 'pawn', 'K': 'king', 'Q': 'queen', 'R': 'rook', 'B': 'bishop', 'N': 'knight'}
 
 
 def count_material(position: Position, color: str) -> int:
@@ -32,6 +35,30 @@ def count_material_imbalance(position: Position, color: str) -> int:
     own_material = count_material(position, color)
     opponents_material = count_material(position, opposite_color(color))
     return own_material - opponents_material
+
+
+def evaluate_exchange_square(position: Position, square: str, init_possible_captures: List[LegalMove]) -> int:
+    """
+    Counts the amount of material that the side to move stands to gain from initiating a series of captures and
+    recaptures on the given square. Both sides "dogpile" onto the square, sending their least valuable pieces to capture
+    first, until one side cannot recapture.
+    :param init_possible_captures:
+    :param square:
+    :param position:
+    :return:
+    """
+    capturing_move = min(init_possible_captures, key=lambda x: MATERIAL_DICT[x.piece_moved])
+    if not capturing_move.is_en_passant_capture():
+        captured_piece = SYMBOL_TO_PIECE[position.look_at_square(square).upper()]
+    else:
+        captured_piece = 'pawn'
+    position_after_capture = branch_from_position(position, capturing_move)
+    possible_recaptures = [move for move in position_after_capture.get_all_legal_moves_for_side_to_move() if move.destination_square == square and move.is_capture()]
+    material_gain = MATERIAL_DICT[captured_piece]
+    if not possible_recaptures:
+        return material_gain
+    else:
+        return material_gain - evaluate_exchange_square(position_after_capture, square, possible_recaptures)
 
 
 def all_legal_piece_moves(position: Position, color: str) -> List[LegalMove]:
@@ -110,23 +137,27 @@ def piece_moves_reduced_by_enemy_pawn_control(unreduced_piece_moves: List[str],
     return [sq for sq in unreduced_piece_moves if sq not in squares_controlled]
 
 
-def evaluate(position: Position, color: str) -> float:
+def evaluate(position: Position) -> float:
     """
-    The evaluation function to be called to evaluate how good or bad a position is for the given side. Assumption is
-    that checkmate or stalemate has not occurred.
+    The evaluation function to be called to evaluate how good or bad a position is for the side that has just moved.
     :param position: Position object
-    :param color: 'white' or 'black'
     :return: float. The higher, the better. 0 means equal position.
     """
-    opposing_side = opposite_color(color)
-    score = count_material_imbalance(position, color)  # MATERIAL IMBALANCE
-    own_pawn_controlled_squares = squares_controlled_by_pawns(position, color)
-    opposing_pawn_controlled_squares = squares_controlled_by_pawns(position, opposing_side)
-    activity = piece_activity(position, color)
-    opposing_activity = piece_activity(position, opposing_side)
+    side_to_move = position.to_move()
+    side_evaluating_for = opposite_color(side_to_move)
+    if not position.get_all_legal_moves_for_color(side_to_move):
+        if position.is_under_check(side_to_move):
+            return CHECKMATE_SCORE
+        else:
+            return 0
+    score = count_material_imbalance(position, side_evaluating_for)  # MATERIAL IMBALANCE
+    own_pawn_controlled_squares = squares_controlled_by_pawns(position, side_evaluating_for)
+    opposing_pawn_controlled_squares = squares_controlled_by_pawns(position, side_to_move)
+    activity = piece_activity(position, side_evaluating_for)
+    opposing_activity = piece_activity(position, side_to_move)
     piece_moves_reduced = piece_moves_reduced_by_enemy_pawn_control(activity, opposing_pawn_controlled_squares)
     opposing_piece_moves_reduced = piece_moves_reduced_by_enemy_pawn_control(opposing_activity,
                                                                              own_pawn_controlled_squares)
     score += ACTIVITY_COUNT_MULTIPLIER * (len(piece_moves_reduced) - len(opposing_piece_moves_reduced))  # LEGAL PIECE MOVES
-    score += get_pawn_control_score(own_pawn_controlled_squares, color) - get_pawn_control_score(opposing_pawn_controlled_squares, opposing_side)  # SQUARES CONTROLLED BY PAWNS
+    score += get_pawn_control_score(own_pawn_controlled_squares, side_evaluating_for) - get_pawn_control_score(opposing_pawn_controlled_squares, side_to_move)  # SQUARES CONTROLLED BY PAWNS
     return score
