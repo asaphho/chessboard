f221d7fa5bbf81ebddfede27ec3444f167b6e583
asaphho <asaphho93@gmail.com>
2024-08-15 09:32:19 +0800

    Made Bot constructor have default value of 0 for fluctuation

--- classes/bot.py
+++ classes/bot.py
@@ -6,7 +6,7 @@ from simple_bot.move_search import choose_best_move
 class Bot:
 
     def __init__(self, evaluation_func: Callable[[Position], Dict[str, float]], breadth: int,
-                 pick_n_threatening: int, fluctuation: float):
+                 pick_n_threatening: int, fluctuation: float = 0):
         self.evaluation_func = evaluation_func
         self.breadth = breadth
         self.aggression = pick_n_threatening
