751728bcea81c8335b25502b853d7e71869ad9fe
Asaph Ho <asaphho@yahoo.com.sg>
2024-08-19 21:11:14 +0800

    Now consults opening book before moving

--- classes/bot.py
+++ classes/bot.py
@@ -1,19 +1,22 @@
+import json
 from typing import Callable, Dict
 from classes.position import Position
 from simple_bot.move_search import choose_best_move, choose_best_move_recursive
+from random import choice
 
 
 class Bot:
 
     def __init__(self, evaluation_func: Callable[[Position], Dict[str, float]], breadth: int = 3,
                  aggression: int = 1, fluctuation: float = 0, assumed_opp_aggresion: int = 1,
-                 ply_depth: int = 4):
+                 ply_depth: int = 4, opening_book_path: str = None):
         self.evaluation_func = evaluation_func
         self.breadth = breadth
         self.aggression = aggression
         self.fluctuation = fluctuation
         self.assumed_opp_aggression = assumed_opp_aggresion
         self.ply_depth = ply_depth
+        self.opening_book_path = opening_book_path
 
     def choose_move(self, position: Position) -> str:
         return choose_best_move(position=position, evaluate=self.evaluation_func, breadth=self.breadth,
@@ -25,3 +28,21 @@ class Bot:
                                           aggression=self.aggression, fluctuation=self.fluctuation,
                                           assumed_opp_aggression=self.assumed_opp_aggression,
                                           ply_depth=self.ply_depth)[0]
+
+    def look_in_opening_book(self, position: Position) -> str:
+        if not self.opening_book_path:
+            return '0000'
+        current_fen = position.generate_fen().rsplit(' ', maxsplit=2)[0]
+        with open(self.opening_book_path, 'r') as readfile:
+            opening_book = json.load(readfile)
+        if current_fen not in opening_book:
+            return '0000'
+        else:
+            return choice(opening_book[current_fen])
+
+    def make_move(self, position: Position) -> str:
+        opening_book_move = self.look_in_opening_book(position)
+        if opening_book_move != '0000':
+            return opening_book_move
+        else:
+            return self.choose_move_recursive(position)
