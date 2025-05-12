e30075c68feafe13ad5df721ed9e42d0d0c69503
Asaph Ho <asaphho@yahoo.com.sg>
2025-05-12 15:04:21 +0800

    Added option for printing moves and game results.

--- bot_training/bot_matchup.py
+++ bot_training/bot_matchup.py
@@ -4,7 +4,7 @@ from typing import Dict
 from simple_bot.bot1.evaluation import quick_evaluate
 
 
-def run_match(training_bot: Bot, opposing_bot: Bot, n_rounds: int = 12) -> str:
+def run_match(training_bot: Bot, opposing_bot: Bot, n_rounds: int = 12, print_moves: bool = False) -> str:
     training_bot_score = 0
     opposing_bot_score = 0
     white_bot = 't'
@@ -12,20 +12,33 @@ def run_match(training_bot: Bot, opposing_bot: Bot, n_rounds: int = 12) -> str:
     for i in range(n_rounds):
         game = Game()
         game_end_condition = game.check_game_end_conditions()
+        if print_moves:
+            if white_bot == 't':
+                print('Bot1 - Bot2')
+            else:
+                print('Bot2 - Bot1')
         while game_end_condition == 'N':
             side_to_move = game.current_position.to_move()
             if side_to_move.lower().startswith('w'):
                 bot_to_play = white_bot
             else:
                 bot_to_play = black_bot
-            game.play_computer_move(training_bot if bot_to_play == 't' else opposing_bot)
+            move_notation = game.play_computer_move(training_bot if bot_to_play == 't' else opposing_bot)
+            if print_moves:
+                print(move_notation)
             game_end_condition = game.check_game_end_conditions()
         if game_end_condition.lower().startswith('white'):
             winning_bot = white_bot
+            if print_moves:
+                print(f'White ({"Bot1" if white_bot == "t" else "Bot2"}) wins.')
         elif game_end_condition.lower().startswith('black'):
             winning_bot = black_bot
+            if print_moves:
+                print(f'Black ({"Bot1" if black_bot == "t" else "Bot2"}) wins.')
         else:
             winning_bot = 'n'
+            if print_moves:
+                print('Game drawn.')
         if winning_bot == 't':
             training_bot_score += 1
         elif winning_bot == 'o':
@@ -33,6 +46,9 @@ def run_match(training_bot: Bot, opposing_bot: Bot, n_rounds: int = 12) -> str:
         else:
             training_bot_score += 0.5
             opposing_bot_score += 0.5
+        if print_moves:
+            print(f'Bot1: {training_bot_score}')
+            print(f'Bot2: {opposing_bot_score}')
         white_bot, black_bot = black_bot, white_bot
         print(f'A game has been completed. ({i+1}/{n_rounds})')
     if training_bot_score > opposing_bot_score:
