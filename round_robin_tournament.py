3cef9197e3e5c6bb83d0827edb08886bc838c972
Asaph Ho <asaph.ho-cy@venture.com.sg>
2025-11-15 20:07:08 +0800

    Added breeding for next generation

--- round_robin_tournament.py
+++ round_robin_tournament.py
@@ -8,9 +8,14 @@ import multiprocessing as mp
 import sys
 from bot_training.round_robin import generate_pairings, rank_all_players
 from bot_training.swiss_tournament import update_round_results
+from bot_training.utils import breed_offspring_from_pool
 
 N_PROCESSES = int(sys.argv[1])
 N_GAMES_PER_MATCH = 4
+MUTATION_STRENGTH = 0.05
+MUTATION_PROBABILITY = 0.1
+ELITES_TO_KEEP = 2
+BREEDING_FOR_NEXT_GEN = 5
 
 
 if __name__ == '__main__':
@@ -18,10 +23,11 @@ if __name__ == '__main__':
         players = json.load(f)
 
     player_configs = [numerify_json_keys(config) for config in players]
-    n_rounds = len(players) - 1
+    n_players = len(players)
+    n_rounds = n_players - 1
 
     curr_results = {}
-    for i in range(len(players)):
+    for i in range(n_players):
         curr_results[i] = []
 
     for i in range(n_rounds):
@@ -38,3 +44,16 @@ if __name__ == '__main__':
     final_rankings = rank_all_players(curr_results)
     print(f'Final results:\n{json.dumps(curr_results)}')
     print(f'Final rankings: {final_rankings}')
+
+    for_breeding = final_rankings[:BREEDING_FOR_NEXT_GEN]
+    configs_for_breeding = [player_configs[i] for i in for_breeding]
+    elites = for_breeding[:ELITES_TO_KEEP]
+    elite_configs = [player_configs[i] for i in elites]
+    offspring = breed_offspring_from_pool(parents=configs_for_breeding,
+                                          n_offspring=n_players - ELITES_TO_KEEP,
+                                          mutation_prob=MUTATION_PROBABILITY,
+                                          mutation_strength=MUTATION_STRENGTH)
+    next_gen = elite_configs + offspring
+    with open(path.join(BOT_TRAINING_DIR, 'round_robin_pool.json'), 'w') as w:
+        w.write(json.dumps(next_gen, indent=4))
+
